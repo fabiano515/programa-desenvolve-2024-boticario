cbd282b34cd73d927d0d553a6165911a8c09680b
Bowie <fabiannoamachado@gmail.com>
2024-03-09 00:53:54 -0300

    Dando mais oportunidades ao jogador

--- projetos_python/adivinhacao.py
+++ projetos_python/adivinhacao.py
@@ -1,19 +1,25 @@
+from random import randrange
+
 print("***********************************")
 print("*Bem vindo ao jogo de adivinhação.*")
 print("***********************************")
 
-numero_secreto = 42
-chute = int(input("Digite um número entre 0 e 100 \n"))
-print("Você digitou ", chute)
+numeroSecreto = 5
+chance = 1
+while (chance <=3):
+    chute = int(input("Digite um número entre 0 e 10 \n"))
+    print(f"Tentativa {chance} de 3.")
+    print("Você digitou ", chute)
 
-acerto = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acerto = chute == numeroSecreto
+    maior = chute > numeroSecreto
+    menor = chute < numeroSecreto
 
-if (acerto):
-    print("Você acertou")
-else:
-    if (maior):
-        print("Seu chute foi maior que o número, você errou!")
-    elif (menor):
-        print("Seu chute foi menor que o número secreto, você errou!")
+    if (acerto):
+        print("Você acertou")
+    else:
+        if (maior):
+            print("Seu chute foi maior que o número, você errou!")
+        elif (menor):
+            print("Seu chute foi menor que o número secreto, você errou!")
+    chance = chance +1
