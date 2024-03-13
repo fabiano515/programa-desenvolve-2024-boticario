a3d5d1fdd4e15f48485104563b425e50be214389
Bowie <fabiannoamachado@gmail.com>
2024-03-12 22:57:57 -0300

    Aumentando a dificuldade e adicionando níveis

--- projetos_python/adivinhacao.py
+++ projetos_python/adivinhacao.py
@@ -4,15 +4,32 @@ print("***********************************")
 print("*Bem vindo ao jogo de adivinhação.*")
 print("***********************************")
 
-numeroSecreto = randrange(0, 11)
-tentativas = 3
+numeroSecreto = randrange(1, 101)
+tentativas = 0
 chance = 1
 
+print("Escolha um nível de dificuldade")
+print("Digite (1) Facil, (2) Médio ou (3) Difícil")
+nivel = int(input("Escolha seu nível :\n"))
+
+if nivel == 1:
+        tentativas = 100
+
+elif nivel == 2:
+       tentativas = 50
+
+else:
+        tentativas = 10
+
 while chance <= tentativas:
-    chute = int(input("Digite um número entre 0 e 10 \n"))
-    print(f"Tentativa {chance} de 3.")
+    chute = int(input("Digite um número entre 1 e 100 \n"))
+    print(f"Tentativa {chance} de {tentativas}.")
     print(f"Você digitou {chute} ")
 
+    if chute < 1 or chute >10:
+        print("Você deve digitar um número entre 1 e 10 \n")
+
+    chance = chance + 1
     acerto = chute == numeroSecreto
     maior = chute > numeroSecreto
     menor = chute < numeroSecreto
@@ -25,4 +42,4 @@ while chance <= tentativas:
             print("Seu chute foi maior que o número secreto, você errou!")
         elif menor:
             print("Seu chute foi menor que o número secreto, você errou!")
-    chance = chance + 1
+
